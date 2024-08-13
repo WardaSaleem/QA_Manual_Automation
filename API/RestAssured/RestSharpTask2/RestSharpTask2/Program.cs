93772a70b104d85111a1379310c8e5a754937419
WardaSaleem <101577993+WardaSaleem@users.noreply.github.com>
2024-08-13 09:57:56 +0300

    .

---
+++ API/RestAssured/RestSharpTask2/RestSharpTask2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace RestSharpTask2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://simple-books-api.glitch.me");
+
+            
+            var request = new RestRequest("/books", Method.Get);
+
+          
+            RestResponse response = client.Execute(request);
+
+            // Check if status code is 200
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                
+                JArray jsonResponse = JArray.Parse(response.Content);
+
+                // Display each book's id and name
+                Console.WriteLine("Books:");
+                foreach (JObject book in jsonResponse)
+                {
+                    int id = (int)book["id"];
+                    string name = (string)book["name"];
+                    Console.WriteLine($"ID: {id}, Name: {name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.ErrorMessage);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
